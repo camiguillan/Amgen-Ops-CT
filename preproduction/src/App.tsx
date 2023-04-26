f31b8f34c38df938119db58597b56316e3b11367
camiguillan <camiguillan@gmail.com>
2023-04-26 11:57:42 -0300

    First commit?
    Co-authored-by: LucasSon98 <LucasSon98@users.noreply.github.com>
    Co-authored-by: valengonzalez8 <valengonzalez8@users.noreply.github.com>

---
+++ preproduction/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Layout from './layout';
+
+function App() {
+  return (
+    <div>
+      
+      <Layout></Layout>
+
+    </div>
+  );
+}
+
+export default App;
