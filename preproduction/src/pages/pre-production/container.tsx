f4fb08dbdfa2292fa410f946f173d88875a6794d
LucasSon98 <lucas.s.son98@gmail.com>
2023-05-08 11:39:26 -0300

    commit

--- preproduction/src/pages/pre-production/container.tsx
+++ preproduction/src/pages/pre-production/container.tsx
@@ -3,7 +3,6 @@ import Card from '../../components/reusable/card';
 function Container() {
     return(
         <div>
-        
             <Card title={'Current Batch Information'} classId={'cbi'} ></Card>
             <Card title={'Changeover Status'} classId={'cs'}></Card>
             <Card title={'Upcoming Lots'} classId={'upl'}></Card>
