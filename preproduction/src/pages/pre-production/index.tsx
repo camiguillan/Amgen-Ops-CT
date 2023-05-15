72081d10a50459391fb44f69e124229bb1891a75
LucasSon98 <lucas.s.son98@gmail.com>
2023-05-15 12:17:00 -0300

    commit

--- preproduction/src/pages/pre-production/index.tsx
+++ preproduction/src/pages/pre-production/index.tsx
@@ -1,12 +1,18 @@
 import Layout from '../../components/reusable/layout';
 import Container from './container';
 import Header from '../../components/reusable/header';
+import { Button } from '@gitlab-rtsensing/component-library';
 
 function PreProduction(){
  return(
     <Layout >
         <Header title1={"AOH"} title2={"Digital Control Tower"}/>
         <Container/>
+        <Button
+            label="Primary Button"
+            onClick={() => {}}
+            type="primary"
+            />
     </Layout>
 
     
