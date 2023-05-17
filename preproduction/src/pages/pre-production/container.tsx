ed6534b0056121c9c333bef0cc5c23d5213a3c36
valengonzalez8 <vale_gonzalez99@hotmail.com>
2023-05-17 10:06:06 -0300

    commit

--- preproduction/src/pages/pre-production/container.tsx
+++ preproduction/src/pages/pre-production/container.tsx
@@ -1,32 +1,71 @@
+import React from 'react';
 import './container.scss';
-import { MetricCard, MetricCardHeader } from '@gitlab-rtsensing/component-library';
+import { MetricCardHeader, TextMetric, Container as Cont, MetricCard } from '@gitlab-rtsensing/component-library';
+import MetricCardContent from '@gitlab-rtsensing/component-library/lib/components/elements/metric-card/metric-card-content';
 
 
 function Container() {
     return(
         <div className='container'>
-            <MetricCard className="mx-2 flex-grow-1 flex-shrink-1" >
-                <MetricCardHeader>
-                    <p className="ops-text-dark ops-fw-bold ops-fs-4">
-                    Current Batch Information
-                    </p>
-                </MetricCardHeader>
-            </MetricCard>
-            <MetricCard className="mx-2 flex-grow-2 flex-shrink-2">
-                {/* <p className='cs-date'>As of 03/25 12:15pm</p> */}
-                <MetricCardHeader>
-                    <p className="ops-text-dark ops-fw-bold ops-fs-4">
-                    Changeover Status
-                    </p>
-                </MetricCardHeader>
-            </MetricCard>
-            <MetricCard className="mx-2 flex-grow-1 flex-shrink-1">
-                <MetricCardHeader>
-                    <p className="ops-text-dark ops-fw-bold ops-fs-4">
-                    Upcoming Lots
-                    </p>
-                </MetricCardHeader>
-            </MetricCard>
+            <Cont className='flex-grow-1 '>
+                <MetricCard className="mx-2 " >
+                    <React.Fragment key=".0">
+                        <MetricCardHeader>
+                            <p className="head ops-text-dark">
+                            Current Batch Information
+                            </p>
+                        </MetricCardHeader>
+                        <MetricCardContent>
+                            <TextMetric
+                                fields={[
+                                    {
+                                    alert: 'Green',
+                                    label: 'Cost',
+                                    name: 'cost',
+                                    value: '$495 - $715M'
+                                    },
+                                    {
+                                    alert: 'Green',
+                                    label: 'Timeline',
+                                    name: 'timeline',
+                                    value: 'Q3 2023'
+                                    },
+                                    {
+                                    alert: 'Yellow',
+                                    label: 'Risk',
+                                    name: 'risk',
+                                    value: 'Construction material constraints'
+                                    },
+                                    {
+                                    alert: 'Green',
+                                    label: 'Scope',
+                                    name: 'scope',
+                                    value: 'No Charge'
+                                    }
+                                ]}
+                                />
+                        </MetricCardContent>
+                    </React.Fragment>
+                </MetricCard>
+            </Cont>
+            <Cont className='flex-grow-1 align-items-stretch'>
+                <MetricCard className="mx-2">
+                    <MetricCardHeader className='d-flex justify-content-between align-items-center'>
+                        <p className="head ops-text-dark">Changeover Status</p>
+                        <p className='cs-date'>As of 03/25 12:15pm</p>
+                    </MetricCardHeader>
+                </MetricCard>
+            </Cont>
+            <Cont className='flex-grow-1 align-items-stretch'>
+                <MetricCard className="mx-2 ">
+                    <MetricCardHeader>
+                        <p className="head ops-text-dark">
+                        Upcoming Lots
+                        </p>
+                    </MetricCardHeader>
+                </MetricCard>
+            </Cont>
+            
         </div>
     );
 }
